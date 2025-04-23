c88bea6221c868503d1d0d7337e5c6d546672f4b
Stephen Pasch <ssp2188@columbia.edu>
2025-04-23 19:04:53 -0400

    added avg pooling layers between conv block

--- models/AutoMat/model.py
+++ models/AutoMat/model.py
@@ -6,25 +6,35 @@ class ConvAutoencoder(nn.Module):
         super(ConvAutoencoder, self).__init__()
         # Encoder
         self.encoder = nn.Sequential(
-            nn.Conv2d(6, 64, 4, stride=2, padding=1),   # -> (64,256,256)
+            nn.Conv2d(6, 64, kernel_size=3, stride=1, padding=1),  # 512x512x6 -> 512x512x64
             nn.ReLU(),
-            nn.Conv2d(64, 128, 4, stride=2, padding=1),  # -> (128,128,128)
+            nn.AvgPool2d(kernel_size=2, stride=2),  # -> (64, 256, 256)
+            
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),  
             nn.ReLU(),
-            nn.Conv2d(128, 256, 4, stride=2, padding=1), # -> (256,64,64)
+            nn.AvgPool2d(kernel_size=2, stride=2),  # -> (128, 128, 128)
+            
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1), 
             nn.ReLU(),
+            nn.AvgPool2d(kernel_size=2, stride=2),  # -> (256, 64, 64)
+            
             nn.Flatten(),
-            nn.Linear(256 * 64 * 64, self.latent_dim) # use product of last used conv2d layer
+            nn.Linear(256 * 64 * 64, latent_dim)
         )
+        
         # Decoder
         self.decoder = nn.Sequential(
-            nn.Linear(self.latent_dim, 256 * 64 * 64), 
+            nn.Linear(latent_dim, 256 * 64 * 64),
             nn.ReLU(),
             nn.Unflatten(1, (256, 64, 64)),
-            nn.ConvTranspose2d(256, 128, 4, stride=2, padding=1),  # -> (128,128,128)
+            
+            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1),  # -> (128, 128, 128)
             nn.ReLU(),
-            nn.ConvTranspose2d(128, 64, 4, stride=2, padding=1),   # -> (64,256,256)
+            
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),  # -> (64, 256, 256)
             nn.ReLU(),
-            nn.ConvTranspose2d(64, 6, 4, stride=2, padding=1), # -> (64,512,512)
+            
+            nn.ConvTranspose2d(64, 6, kernel_size=4, stride=2, padding=1),  # -> (6, 512, 512)
             nn.Sigmoid()  # For pixel values in [0,1]
         )
 
